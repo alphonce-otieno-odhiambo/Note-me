f733c92e65b403485c97ae18b773d389afdaf3e6
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-14 00:01:04 +0300

    install corseheaders

--- note_me/urls.py
+++ note_me/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name='getroutes'),
     path('notes/', views.getNotes, name='getNotes'),
-    path('note/<str:pk>/', views.getNotes, name='note'),
+    path('note/<str:pk>/', views.note, name='note'),
 ]
