836dfb9780de58c051f986c6bcfe628f33195c07
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-13 21:21:43 +0300

    create superuser and register class

--- note_me/models.py
+++ note_me/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class Note(models.Modal):
+class Note(models.Model):
     body=models.TextField(max_length=500, null=True, blank=True),
     update = models.DateTimeField(auto_now=True),
     created = models.DateField(auto_now_add=True),
