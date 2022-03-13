f2e2805823a428a83bfb84382980357b861f6f9c
alphonce-otieno-odhiambo <alphonce.odhiambo@student.moringaschool.com>
2022-03-13 21:12:23 +0300

    create the note class database

--- note_me/models.py
+++ note_me/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Note(models.Modal):
+    body=models.TextField(max_length=500, null=True, blank=True),
+    update = models.DateTimeField(auto_now=True),
+    created = models.DateField(auto_now_add=True),
+
+    def __setattr__(self):
+        return self.body[0:50]
